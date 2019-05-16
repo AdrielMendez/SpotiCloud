168485ffb53c36279f8bf1d7ba080c779ec60648
adriel315 <adriel315@gmail.com>
2019-05-16 14:44:03 -0400

    added auth throughout the site, and removed some redundant templates/pages

--- word_cloud/auth.py
+++ word_cloud/auth.py
@@ -6,20 +6,18 @@ from urllib.parse import quote
 import requests
 import json
 
+
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for, current_app
 )
 
 
-
-
-
 bp = Blueprint('auth', __name__)
-# , url_prefix='/auth'
+
 
 #  Client Keys
-CLIENT_ID = ""
-CLIENT_SECRET = ""
+CLIENT_ID = "53f83824eefa4ff2ab7f43f2e530ba90"
+CLIENT_SECRET = "a1fff141754e45358e2a925ce8772c0b"
 
 # Spotify URLS
 SPOTIFY_AUTH_URL = "https://accounts.spotify.com/authorize"
@@ -52,23 +50,29 @@ auth_query_parameters = {
 @bp.route("/login")
 def login():
     # Auth Step 1: Authorization
+    template = 'authLayout.html'
     name = "login page"
     url_args = "&".join(["{}={}".format(key, quote(val)) for key, val in auth_query_parameters.items()])
     auth_url = "{}/?{}".format(SPOTIFY_AUTH_URL, url_args)
-    return render_template('auth.html', spot_link=auth_url, name=name)
+    return redirect(auth_url)
+    # display = True
+    # if not loggedIn():
+    #     display = False
+    # return render_template(template, spot_link=auth_url, name=name, display=display)
 
 
 
 @bp.route('/logout')
 def logout():
-    name = "spotify logout"
+    from .wrdcld import home
+
     session.clear()
-    template = redirect(url_for('wrdcld.home'))
-    return render_template(template, name=name)
+    return home()
 
 
 @bp.route("/callback/q")
 def callback():
+    from .wrdcld import wordCloud
     # Auth Step 4: Requests refresh and access tokens
     auth_token = request.args['code']
     code_payload = {
@@ -78,6 +82,7 @@ def callback():
         'client_id': CLIENT_ID,
         'client_secret': CLIENT_SECRET,
     }
+
     post_request = requests.post(SPOTIFY_TOKEN_URL, data=code_payload)
 
     # Auth Step 5: Tokens are Returned to Application
@@ -88,35 +93,59 @@ def callback():
     expires_in = response_data["expires_in"]
 
     # Auth Step 6: Use the access token to access Spotify API
-    authorization_header = {"Authorization": "Bearer {}".format(access_token)}
+    auth_header = {"Authorization": "Bearer {}".format(access_token)}
 
-    # Get profile data
-    user_profile_api_endpoint = "{}/me".format(SPOTIFY_API_URL)
-    profile_response = requests.get(user_profile_api_endpoint, headers=authorization_header)
-    profile_data = json.loads(profile_response.text)
+    # # Get profile data
+    # user_profile_api_endpoint = "{}/me".format(SPOTIFY_API_URL)
+    # profile_response = requests.get(user_profile_api_endpoint, headers=authorization_header)
+    # profile_data = json.loads(profile_response.text)
+    #
+    # # Get user playlist data
+    # playlist_api_endpoint = "{}/playlists".format(profile_data["href"])
+    # playlists_response = requests.get(playlist_api_endpoint, headers=authorization_header)
+    # playlist_data = json.loads(playlists_response.text)
+    #
+    # # Combine profile and playlist data to display
+    # display_arr = [profile_data] + playlist_data["items"]
 
-    # Get user playlist data
-    playlist_api_endpoint = "{}/playlists".format(profile_data["href"])
-    playlists_response = requests.get(playlist_api_endpoint, headers=authorization_header)
-    playlist_data = json.loads(playlists_response.text)
-
-    # Combine profile and playlist data to display
-    display_arr = [profile_data] + playlist_data["items"]
-
-
+    session['auth_header'] = auth_header
     session['access_token'] = access_token
     session['refresh_token'] = refresh_token
 
-    return render_template('wrdcld.html', sorted_array=display_arr, name=name)
+    return wordCloud()
 
 
+# def login_required(view):
+#     @functools.wraps(view)
+#     def wrapped_view(**kwargs):
+#         if session['access_token'] is None:
+#             return redirect(url_for('auth.login'))
+#         return view(**kwargs)
+#     return wrapped_view
 
-def login_required(view):
-    @functools.wraps(view)
-    def wrapped_view(**kwargs):
-        if session['access_token'] is None:
-            return redirect(url_for('auth.login'))
-        return view(**kwargs)
-    return wrapped_view
 
+# def loggedIn() -> bool:
+#     granted = False
+#     if 'access_token' in session:
+#         granted = True
+#     return granted
+
+
+# not meant to be deployed, just didnt want to get rid of the logic just yet
+def getplaylist():
+    # Get user playlist data
+    if 'auth_header' in session:
+        auth_header = session['auth_header']
+
+        user_endpoint = "{}/me".format(SPOTIFY_API_URL)
+        profile_response = requests.get(user_endpoint, headers=auth_header)
+        profile_data = json.loads(profile_response.text)
+
+        playlist_api_endpoint = "{}/playlists".format(profile_data["href"])
+        playlists_response = requests.get(playlist_api_endpoint, headers=auth_header)
+        playlist_data = json.loads(playlists_response.text)
+
+        display_arr = [profile_data] + playlist_data["items"]
+
+        return playlist_data
 
--- word_cloud/wrdcld.py
+++ word_cloud/wrdcld.py
@@ -1,8 +1,11 @@
-from flask import (
-    Blueprint, flash, g, redirect, render_template, request, url_for
-)
-from .auth import login_required
 from werkzeug.exceptions import abort
+import spotipy.util as util
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, url_for, session, current_app
+)
+from .auth import getplaylist
+
+
 
 
 bp = Blueprint('wrdcld', __name__)
@@ -10,21 +13,30 @@ bp = Blueprint('wrdcld', __name__)
 
 @bp.route('/')
 @bp.route('/home')
-def home(page_name=None):
-    template = "wrdcld.html"
+def home():
+    template = "wrdcld/home.html"
     page_name = "Home"
     return render_template(template, name=page_name)
 
 @bp.route('/wordCloud')
-@login_required
 def wordCloud():
-    template = "wrdcld.html"
+    """connection to WordCloud class is done here"""
+    template = "wrdcld/wrdcld.html"
     page_name = "WordCloud Creation"
-    return render_template(template, name=page_name)
+    playlist = getplaylist()
+    if 'access_token' not in session:
+        return redirect('auth.login')
+    return render_template(template, name=page_name, playlist=playlist)
 
 
 @bp.route('/about/')
-def about(page_name=None):
-    template = "about.html"
+def about():
+    template = "wrdcld/about.html"
     page_name = "About"
-    return render_template(template, name=page_name)
+    playlist = getplaylist()
+    if 'auth_header' in session:
+        return render_template(template, name=page_name, playlist=playlist) # if logged in display logged in version of about.html
+    else:
+        inhere = "in here"
+        template = "loggedoutAbout.html"
+        return render_template(template, name=page_name, playlist=playlist, state=inhere) # else display logged out version
